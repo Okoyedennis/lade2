65600a72ae7c86501a1d74d0fa9f23616da8cdb8
Dennis <okoyedennis7@gmail.com>
2022-10-13 13:00:22 +0100

    ok

---
+++ src/main/java/com/example/apply_for_school/dto/ApplyDto.java
@@ -0,0 +1,24 @@
+package com.example.apply_for_school.dto;
+
+import com.example.apply_for_school.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApplyDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String stateOfOrigin;
+    private Gender gender;
+    private LocalDate dateOfBirth;
+    private Long BVN;
+}
